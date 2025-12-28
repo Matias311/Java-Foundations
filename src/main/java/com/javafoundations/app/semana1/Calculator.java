65f04f2cc026c4d681630b18e6104d07eae53e58
Matias Arias <matiasarias3103@gmail.com>
2025-12-28 19:43:38 -0300

    Mejoras a la calculadora y a los test

--- src/main/java/com/javafoundations/app/semana1/Calculator.java
+++ src/main/java/com/javafoundations/app/semana1/Calculator.java
@@ -44,6 +44,9 @@ public class Calculator {
    * @return int
    */
   public int division(int num1, int num2) {
+    if (num2 == 0) {
+      throw new IllegalArgumentException("Division by zero");
+    }
     return num1 / num2;
   }
 }
--- src/test/java/com/javafoundations/app/semana1/CalculatorTest.java
+++ src/test/java/com/javafoundations/app/semana1/CalculatorTest.java
@@ -3,70 +3,67 @@ package com.javafoundations.app.semana1;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("Test for calculator class")
 public class CalculatorTest {
 
-  private static Calculator calculator;
+  private Calculator calculator;
 
-  @BeforeAll
-  static void setUp() {
-    calculator = new Calculator();
+  @BeforeEach
+  void setUp() {
+    this.calculator = new Calculator();
   }
 
   @Test
   @DisplayName("Sum two by two and verify the result")
   void sumTwoByTwo() {
-    assertEquals(4, calculator.add(2, 2), () -> "Error, the result is: " + calculator.add(2, 2));
+    int result = calculator.add(2, 2);
+    assertEquals(4, result, () -> "Error, the result is: " + result);
   }
 
   @Test
   @DisplayName("Sum (minus) two by two and verify the result")
   void sumMinusTwoByTwo() {
-    assertEquals(0, calculator.add(-2, 2), () -> "Error, the result is: " + calculator.add(-2, 2));
+    int result = calculator.add(-2, 2);
+    assertEquals(0, result, () -> "Error, the result is: " + result);
   }
 
   @Test
   @DisplayName("Subtract zero by two and verify the result")
   void subtractZeroByTwo() {
-    assertEquals(
-        -2,
-        calculator.subtraction(0, 2),
-        () -> "Error, the result is: " + calculator.subtraction(0, 2));
+    int result = calculator.subtraction(0, 2);
+    assertEquals(-2, result, () -> "Error, the result is: " + result);
   }
 
   @Test
   @DisplayName("Subtract (negative) two by (negative) four")
   void subtractNegativeTwoByNegativeFour() {
-    assertEquals(
-        2,
-        calculator.subtraction(-2, -4),
-        () -> "Error, the result is: " + calculator.subtraction(-2, -4));
+    int result = calculator.subtraction(-2, -4);
+    assertEquals(2, result, () -> "Error, the result is: " + result);
   }
 
   @Test
   @DisplayName("Multiply six by four and verify the result")
   void multiplySixByFour() {
-    assertEquals(
-        24,
-        calculator.multiplication(6, 4),
-        () -> "Error, the result is: " + calculator.multiplication(6, 4));
+    int result = calculator.multiplication(6, 4);
+    assertEquals(24, result, () -> "Error, the result is: " + result);
   }
 
   @Test
   @DisplayName("Division of six by three and verify the result")
   void divisionSixByThree() {
-    assertEquals(
-        2, calculator.division(6, 3), () -> "Error: the result is " + calculator.division(6, 3));
+    int result = calculator.division(6, 3);
+    assertEquals(2, result, () -> "Error: the result is " + result);
   }
 
   @Test
   @DisplayName("Division by zero (six divide by zero)")
   void divisionSixByZeroErrorException() {
-    Exception exception = assertThrows(ArithmeticException.class, () -> calculator.division(6, 0));
-    assertEquals("/ by zero", exception.getMessage());
+    Exception exception =
+        assertThrows(IllegalArgumentException.class, () -> calculator.division(6, 0));
+    assertEquals("Division by zero", exception.getMessage());
   }
 }
